3851f46f592fbf40662347e46c4961b3b1e93d21
faridunBegijonov <faridunBegijonov010@gmail.com>
2023-03-05 12:21:06 +0500

    config src

---
+++ src/i18n.ts
@@ -0,0 +1,31 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+const resources = {
+  ru: {
+    translation: {
+      "Welcome to React": "Welcome to React and react-i18next",
+    },
+  },
+  en: {
+    translation: {
+      "Welcome to React": "Bienvenue à React et react-i18next",
+    },
+  },
+  uzb: {
+    translation: {
+      "Welcome to React": "Bienvenue à React et react-i18next",
+    },
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "ru",
+
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
