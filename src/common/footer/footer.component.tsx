2f7bf63aaa979be705ff26128f50efdeddb76066
faridunBegijonov <faridunBegijonov010@gmail.com>
2023-03-05 14:58:37 +0500

    home end

---
+++ src/common/footer/footer.component.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Container, Typography } from "@mui/material";
+import "./footer.style.scss";
+import i18n from "../../i18n";
+
+export const Footer: React.FC = () => {
+  const changeLanguage = (l: string) => {
+    i18n.changeLanguage(l);
+  };
+  return (
+    <footer className="flex items-center justify-between fixed w-[100%] bottom-[25px] px-[20px] footerContent ">
+      <div className="flex items-center flex-wrap ">
+        <a
+          className="linkFooter mr-[15px]"
+          href="https://www.facebook.com/gravity.tj"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Facebook
+        </a>
+        <a
+          className="linkFooter mr-[15px]"
+          href="https://www.instagram.com/gravity.tj/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Instagram
+        </a>
+        <a
+          className="linkFooter mr-[15px]"
+          href="https://t.me/gravity_tj"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Telegram
+        </a>
+        <a
+          className="linkFooter"
+          href="https://www.linkedin.com/company/gravity-studio-tj/about/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Linkedin
+        </a>
+      </div>
+
+      <div className="flex items-center contentLFooter ">
+        <Typography
+          onClick={() => changeLanguage("ru")}
+          sx={{ mr: 2, color: "rgba(38, 38, 38, 0.5)", cursor: "pointer" }}
+        >
+          Ru
+        </Typography>
+        <Typography
+          onClick={() => changeLanguage("en")}
+          sx={{ mr: 2, color: "rgba(38, 38, 38, 0.5)", cursor: "pointer" }}
+        >
+          En
+        </Typography>
+        <Typography
+          onClick={() => changeLanguage("uzb")}
+          sx={{ color: "rgba(38, 38, 38, 0.5)", cursor: "pointer" }}
+        >
+          Uzb
+        </Typography>
+      </div>
+    </footer>
+  );
+};
--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navbar } from "../../common/navbar/navbar.component";
 import { Container, Typography } from "@mui/material";
 import logoHome from "../../assets/home/homeLogo.svg";
 import "./home.style.scss";
 import { useTranslation } from "react-i18next";
+import { Footer } from "../../common/footer/footer.component";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export const Home: React.FC = () => {
   const { t } = useTranslation();
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <>
       <Navbar />
       <Container maxWidth="xl">
         <div className="h-[70vh] flex items-center justify-between relative contentHome ">
           <Typography
+            data-aos="fade-up"
+            data-aos-duration="1000"
             className="textHome"
             variant="h3"
             sx={{ width: "70%", fontWeight: "400" }}
@@ -26,6 +36,7 @@ export const Home: React.FC = () => {
           />
         </div>
       </Container>
+      <Footer />
     </>
   );
 };
