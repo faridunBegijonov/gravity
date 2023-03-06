c3a41b7afc184a8df575a400556c9c8d77eac97f
faridunBegijonov <faridunBegijonov010@gmail.com>
2023-03-06 17:24:56 +0500

    delete appJs

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import logo from "./logo.svg";
-import { Navbar } from "./common/navbar/navbar.component";
-
-function App() {
-  return <Navbar />;
-}
-
-export default App;
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import React from "react";
 import { Home } from "../pages/home/home.component";
 import { Studio } from "../pages/studio/studio.component";
