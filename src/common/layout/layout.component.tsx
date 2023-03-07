6125aed869f184dd14f0d4a2bbfe7e5e6f43babe
faridunBegijonov <faridunBegijonov010@gmail.com>
2023-03-07 18:30:02 +0500

    darkMode

---
+++ src/common/layout/layout.component.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar } from "../navbar/navbar.component";
+import { Footer } from "../footer/footer.component";
+
+export const Layout: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
