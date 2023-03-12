0811536e2de77d785d5572725e544c6142bf5e93
faridunBegijonov <faridunBegijonov010@gmail.com>
2023-03-12 17:43:31 +0500

    cases style

--- src/components/cases/itemCases.component.tsx
+++ src/components/cases/itemCases.component.tsx
@@ -1,35 +1,37 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { Link } from "react-router-dom";
+import "./cases.style.scss";
 
 interface IItemCases {
-    img: string;
-    spanName: string;
-    spanDes: string;
-    spanTex: string;
-    path: string;
+  src: string;
+  name: string;
+  title: string;
+  description: string;
+  technology: string;
+  path: string;
 }
 
-export const ItemCases: React.FC<IItemCases> = ({ img, spanName, spanDes, spanTex, path }: IItemCases) => {
-    return (
-        <div className="amb">
-            <img src={img} alt="AmbImg" />
-            <p>Амонатбонк</p>
-            <Link to={path}>
-                <div className="bgAmb hover:bg-[#16a34a]/50">
-                    <div className="flex flex-col">
-                        <span className="text-[22px] mb-[8px] ">
-                            {spanName}
-                        </span>
-                        <span className="text-[18px] mb-[8px]">
-                            {spanDes}
-                        </span>
-                        <span>
-                            {spanTex}
-                        </span>
-                    </div>
-                </div>
-            </Link>
+export const ItemCases: React.FC<IItemCases> = ({
+  src,
+  name,
+  path,
+  title,
+  description,
+  technology,
+}: IItemCases) => {
+  return (
+    <div className="amb">
+      <img src={src} alt="AmbImg" />
+      <p>{name}</p>
+      <Link to={path}>
+        <div className="bgAmb hover:bg-[#16a34a]/50">
+          <div className="flex flex-col">
+            <span className="text-[22px] mb-[8px] ">{title}</span>
+            <span className="text-[18px] mb-[8px]">{description}</span>
+            <span>{technology}</span>
+          </div>
         </div>
-
-    )
-}
+      </Link>
+    </div>
+  );
+};
