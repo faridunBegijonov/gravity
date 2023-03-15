f5b13d76cdc4136abd3c06af3ce2eb8ba7c90ae5
Subhoniddin <subhoniddinodinaev@gmail.com>
2023-03-15 12:58:30 +0500

    create startproject component and rout

---
+++ src/components/startprojct/startproject.component.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import { Layout } from '../../common/layout/layout.component';
+
+const StartProject = () => {
+  return (
+    <Layout>
+      <Box maxWidth='xl'>Start Project</Box>
+    </Layout>
+  );
+};
+
+export default StartProject;
---
+++ src/pages/startproject/startproject.page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Layout } from '../../common/layout/layout.component';
+import StartProject from '../../components/startprojct/startproject.component';
+
+const StartProjectPage = () => {
+  return <StartProject />;
+};
+
+export default StartProjectPage;
