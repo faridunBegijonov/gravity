2a16353c107da378b58786722d0263371f088db3
Subhoniddin <subhoniddinodinaev@gmail.com>
2023-03-29 17:02:58 +0500

    add itemcasescomponent

--- src/components/itemCasesComponent/itemCasesComponent.tsx
+++ src/components/itemCasesComponent/itemCasesComponent.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
+import { Container } from '@mui/material';
 import { Layout } from '../../common/layout/layout.component';
-export const ItemCasesComponent: React.FC = ( ) => {
-    return (
-        <Layout>
-            Cases item
-        </Layout>
-    )
-}
+import { Navbar } from '../../common/navbar/navbar.component';
+import amb_bg from '../../assets/ceses/amb/amb_bg.png';
+
+export const ItemCasesComponent: React.FC = () => {
+  return (
+    <div className='bg-[#08AB68] h-screen w-full amb_bg'>
+      <Navbar />
+    </div>
+  );
+};
